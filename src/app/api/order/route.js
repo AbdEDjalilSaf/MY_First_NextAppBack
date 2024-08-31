b938fa80fcab3504e54995b6ccbf5319f500a0be
Abdo saf <noodsaf01@gmail.com>
2024-08-31 10:56:13 +0100

    first App Back

--- src/app/api/order/route.js
+++ src/app/api/order/route.js
@@ -1,10 +1,15 @@
-import { createAdmineClient } from "@/lib/appwrite.config"
+import { createAdmineClient } from "@/lib/appwrite.config";
 
-export async function GET(requist) {
+export async function GET(request) {
+    try {
 const {database} = await createAdmineClient();
 const { documents: order, total} = await database.listDocuments(
     process.env.NEXT_PUBLIC_DATABASE_ID,
     process.env.NEXT_PUBLIC_COLLECTION_ORDER
 )
     return Response.json({order,total});
+} catch (error){
+console.error('Error fetching data : ', error);
+return Response.json({error: 'Faild to fetch data'}, {status: 500});
+};
 } 
