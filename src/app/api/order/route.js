2160b7175ca450885fb2a546c9274377f60a7e7f
Abdo saf <noodsaf01@gmail.com>
2024-09-01 12:06:28 +0100

    first App Back

--- src/app/api/order/route.js
+++ src/app/api/order/route.js
@@ -3,13 +3,26 @@ import { createAdmineClient } from "@/lib/appwrite.config";
 export async function GET(request) {
     try {
 const {database} = await createAdmineClient();
+
+if(!database){
+    console.log(' ----------------------------------------- your database not working');
+
+}
+
+const databaseId = process.env.NEXT_PUBLIC_DATABASE_ID;
+const collectionId = process.env.NEXT_PUBLIC_COLLECTION_ORDER;
+
+if (!databaseId || !collectionId) {
+  throw new Error('Environment variables for database ID or collection ID are missing.');
+}
+
 const { documents: order, total} = await database.listDocuments(
-    process.env.NEXT_PUBLIC_DATABASE_ID,
-    process.env.NEXT_PUBLIC_COLLECTION_ORDER
+    databaseId,
+    collectionId
 )
     return Response.json({order,total});
 } catch (error){
-console.error('Error fetching data : ', error);
+console.error(' --------------------------------------------------------  Error fetching data : ', error);
 return Response.json({error: 'Faild to fetch data'}, {status: 500});
 };
 } 
