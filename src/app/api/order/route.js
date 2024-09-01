ba5b81f484e8afb54fbb985ce78ef19c920d5670
Abdo saf <noodsaf01@gmail.com>
2024-09-01 23:01:25 +0100

    first App Back

--- src/app/api/order/route.js
+++ src/app/api/order/route.js
@@ -1,4 +1,6 @@
 import { createAdmineClient } from "@/lib/appwrite.config";
+import { NextResponse } from 'next/server';
+
 
 export async function GET(request) {
     try {
@@ -13,16 +15,20 @@ const databaseId = process.env.NEXT_PUBLIC_DATABASE_ID;
 const collectionId = process.env.NEXT_PUBLIC_COLLECTION_ORDER;
 
 if (!databaseId || !collectionId) {
-  throw new Error('Environment variables for database ID or collection ID are missing.');
+    console.log(' ----------------------------------------- your database not working');
 }
 
 const { documents: order, total} = await database.listDocuments(
     databaseId,
     collectionId
-)
-    return Response.json({order,total});
+);
+if (!order || !total) {
+    console.log(' ----------------------------------------- your roder and total not working');
+}
+
+    return NextResponse.json({order,total});
 } catch (error){
 console.error(' --------------------------------------------------------  Error fetching data : ', error);
-return Response.json({error: 'Faild to fetch data'}, {status: 500});
+return NextResponse.json({error: 'Faild to fetch data'}, {status: 500});
 };
 } 
